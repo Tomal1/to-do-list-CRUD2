0e41c7e8170a6408dd271c92ce74ddfd495d54c1
MI <minhazulislam@Minhazuls-MacBook-Air.local>
2023-06-24 22:22:05 +0100

    dotenv, cors installed and  all routes created

--- server/server.js
+++ server/server.js
@@ -1,12 +1,86 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 3001;;
+const PORT = process.env.PORT || 3001;
+
+//npm i dotenv
+require("dotenv").config();
+//npm i cors (gives permision for front end to access backend)
+const cors = require("cors");
 
 const app = express();
 
+app.use(
+  cors({
+    // this is needed in express server to give permission
+    origin: "http://127.0.0.1:5501",
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+// express by default dose not use json, this allow it
+app.use(express.json());
+
+const db = require("./config/connection");
+
 app.get("/", (req, res) => {
-    res.json("hello");
+  const sql = "SELECT * FROM input";
+
+  db.query(sql, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      return res.json(data);
+    }
   });
+});
+
+app.post("/post", (req, res) => {
+  const sql = "INSERT INTO input (toDos) VALUE (?)";
+  const values = [req.body.toDos];
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      return res.json(data);
+    }
+  });
+});
+
+app.put("/update", (req, res) => {
+  const sql = "UPDATE input SET toDos = ? WHERE id = ?";
+
+  const values = [
+    //must be in same order as sql (so toDos first and then id)
+    req.body.toDos,
+    req.body.id,
+  ];
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      return res.json(data);
+    }
+  });
+});
+
+app.delete("/delete", (req, res) => {
+  const values = [req.body.id];
+
+  const sql = `DELETE FROM input WHERE id = ?`; // Always use backticks when not hard coding
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(data);
+      return res.json("deleted the todo successfully");
+    }
+  });
+});
+
+app.use(express.static("public"));
 
 app.listen(PORT, () =>{
     console.log(`listning to port ${PORT}`);
