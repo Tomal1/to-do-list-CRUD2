b227688dbb4261fa275099faac2b6fa75232c667
MI <minhazulislam@Minhazuls-MacBook-Air.local>
2023-06-25 22:45:03 +0100

    .env file successfully stores db data and mysql installed

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,12 +5,13 @@ require("dotenv").config();
 
 const db = mysql.createConnection(
   {
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST
+    database : process.env.DB_NAME,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    host : process.env.DB_HOST
   },
   console.log("Connected to mariaDB database")
 );
 
 module.exports = db;
+
