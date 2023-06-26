5469c0f8ad0e8c78a38ab7ef6080641daf7a77cb
MI <minhazulislam@Minhazuls-MacBook-Air.local>
2023-06-26 20:35:33 +0100

    port added to database connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -9,6 +9,7 @@ const db = mysql.createConnection(
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     host : process.env.DB_HOST,
+    port : "3306"
   },
   console.log("Connected to mariaDB database")
 );
